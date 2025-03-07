73cd36edca325754997d27fe261606a0a6d80983
nk-rifat <nk.rifat76@gmail.com>
2025-03-07 21:51:44 +0600

    corrected

--- src/Components/layout-component/Navbar.jsx
+++ src/Components/layout-component/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
                 navigate('/');
             })
             .catch(error => {
-                
+                console.log(error)
             })
     };
 
